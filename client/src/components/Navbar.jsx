dde9b679fecbf91123e673a0e2de6855808ee3f0
Clemente Nogueira <git.3gear@gmail.com>
2022-03-25 23:49:36 +0000

    The Navbar component mobile style was created.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
@@ -13,7 +13,7 @@ const NavbarItem = ({ title, classProps }) => {
 }
 
 const Navbar = () => {
-    const [toggleMenu, setToggleMenu] = React.useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -26,24 +26,23 @@ const Navbar = () => {
                 <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
                     Login
                 </li>
-                <div className="flex relative">
-                    {toggleMenu
-                        ? <AiOutlineClose fonteSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/>
-                        : <HiMenuAlt4 fonteSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
-                    }
-                    {toggleMenu && (
-                        <ul
-                            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
-                        >
-                            <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-                            {["Market", "Exchange", "Tutorials", "Wallets"].map(
-                                (item, index) => <NavbarItem key={item + index} title={item} classprops="my-2 text-lg" />,
-                            )}
-                        </ul>
-                    )}
-                </div>
             </ul>
+            <div className="flex relative">
+                {toggleMenu
+                ? <AiOutlineClose fonteSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/>
+                : <HiMenuAlt4 fonteSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
+                }
+                {toggleMenu && (
+                <ul className="bg-fuchsia-900 z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in" >
+                    <li className="text-xl w-full my-2">
+                        <AiOutlineClose onClick={() => setToggleMenu(false)} className="cursor-pointer" />
+                    </li>
+                        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                        <NavbarItem key={item + index} title={item} classProps={"my-2 text-lg"}/> ))}
+                </ul>
+                )}
+            </div>
         </nav>
     );
 }
